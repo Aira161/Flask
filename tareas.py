aedbc5292070cddd1984b36ddf71de54aaf84ea8
aira <a.solanop@uniandes.edu.co>
2024-08-12 20:26:11 -0500

    cola mensajes

---
+++ tareas.py
@@ -0,0 +1,11 @@
+from celery import Celery
+
+#guardar el log de un logueo en la aplicación
+
+celery_app = Celery(__name__, broker='redis:://localhost:6379/0')
+
+@celery_app.task() #que la funcion va a pasar a traves de la cola es necesario el decoartor
+def registrar_log(usuario, fecha):
+    with open('log_signin.txt', 'a+') as file: #a para crear lineas + crear  el archivo si no existe
+        file.write('{} - inicio de sesion: {} \n'.format(usuario, fecha))
+        
